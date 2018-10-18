80722b99040397ed3a55d58339e708cd8e3510dd
Kenta Nogami <knt.nogami@gmail.com>
2018-10-18 15:56:56 +0900

    SVGで文字化けする現象の対策

--- adoc2pdf.bat
+++ adoc2pdf.bat
@@ -7,6 +7,7 @@ SET OPTIONS=
 SET OPTIONS=%OPTIONS% -r asciidoctor-diagram
 SET OPTIONS=%OPTIONS% -r asciidoctor-pdf-cjk
 SET OPTIONS=%OPTIONS% -r asciidoctor-pdf-cjk-kai_gen_gothic
+SET OPTIONS=%OPTIONS% -r ./svg_cjk.rb
 SET OPTIONS=%OPTIONS% -a pdf-style=KaiGenGothicJP@
 SET OPTIONS=%OPTIONS% -a source-highlighter=coderay@
 CALL bundle exec asciidoctor-pdf -D output %OPTIONS% "%FILENAME%.adoc"
